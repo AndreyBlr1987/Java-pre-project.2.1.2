d30922d21ee3e8c81d88925952ea23fdcc1eb296
Melnik Andrey <melnik_blr1987@mail.ru>
2021-12-04 01:10:13 +0300

    !

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,5 +9,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld newBean = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat firstCat = (Cat) applicationContext.getBean("cat");
+        Cat secondCat = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean == newBean);
+        System.out.println(firstCat == secondCat);
     }
 }
